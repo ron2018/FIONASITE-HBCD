c71f4f42bc08a214d2e1a0223941aa7ea52bc274
ron2018 <33551190+ron2018@users.noreply.github.com>
2023-03-23 11:18:58 -0700

    Create sendSiemensKSPACE.sh

---
+++ server/bin/sendSiemensKSPACE.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+#
+# For HBCD: Siemens k-space data from an MRI scanner is stored as .dat files and name convention is following:
+# Seriese description is at the end of filename:  meas_MID00258_FID00293_T2w_SPACE.dat 
+#   meas_MIDXXXXX_FIDXXXXX_PROTOCOL_NAME.dat
+# 
+# This script tries to rename and pack the files as following: 
+# for series:  rawdata_SUID_suid_SeUID_seuid.zip
+# compress Extra_files as rawdata_SUID_suid_Extra_files.zip
+# Compress Text files as rawdata_SUID_suid_Text_files.zip
+# 
+# Fast compression is enabled if you have pigz installed.
+#
+# This script should be run by user processing (creates mount point).
+#
+# 1 2 * * * /var/www/html/server/bin/sendPhilipsKSPACE.sh >> /var/www/html/server/logs/sendPhilipsKSPACE.sh.log 2>&1
+#
+
+# A .dat file needs to be at least that many seconds old (modification time) before it will be copied
+oldtime=15
+
+echo "starting sendSiemensKSPACE.sh *** "
+#
+# check the user account, this script should run as root
+#
+if [[ $USER !=  "processing" ]]; then
+   echo "This script must be run by the processing user"
+   exit 1
+fi
+
+SERVERDIR=`dirname "$(readlink -f "$0")"`/../
+user=`cat /data/config/config.json | jq -r ".SERVERUSER"`
+
+endpoint=`cat /data/config/config.json | jq -r ".UMNSERVER"`
+if [ "$project" != "" ]; then
+    endpoint=`cat /data/config/config.json | jq -r ".SITES.${project}.UMNSERVER"`
+fi
+echo "Endpoint selected: $endpoint"
+
+token==`cat /data/config/config.json | jq -r ".CONNECTION"`
+if [ "$token" == "" ]; then
+   echo "CONNECTION string is missing from /data/config/config.json file"
+   exit
+fi
+
+#
+# Make sure this script runs only once (each .zip file is about 30G).
+# We will wait until one run is done before we attempt the next.
+#
+thisscript=`basename "$0"`
+for pid in $(/usr/sbin/pidof -x "$thisscript"); do
+    if [ $pid != $$ ]; then
+        echo "[$(date)] : ${thisscript} : Process is already running with PID $pid"
+        exit 1
+    fi
+done
+
+# given error message, and exiting with an error code.
+function error_exit {
+    echo
+    echo "$@"
+    exit 1
+}
+
+log=${SERVERDIR}/logs/sendKspaceFiles.log
+if [ ! -f "$log" ]; then
+   touch "$log"
+fi
+
+kspaceDatLocations="/data/site/kspace/"
+cd  $kspaceDatLocations
+
+#
+# now go through all the files on the /data/site/kspace/ folder
+#
+find ${kspaceDatLocations} -maxdepth 1 -type d -print0 | while read -d $'\0' fdir
+do
+  #tar -xf $file
+  # pattern is : 20221213_1.3.46.670589.11.45002.5.0.15852.2022121307120758000.tgz
+  # tar the file
+  #filedir=$(echo `basename "$file"` | sed "s/.tgz//")
+  
+  filedir=$(echo `basename "$fdir"`);
+  echo $filedir;
+
+  if [ ${filedir} = "kspace" ]; then
+	continue;
+  fi
+  if [ ${filedir} = "umn" ]; then
+	continue;
+  fi
+  if [ ${filedir} = "outbox" ]; then
+       continue;
+  fi
+  if [ ${filedir} = "badkspace" ]; then
+       continue;
+  fi
+  if [ ${filedir} = "processed" ]; then
+       continue;
+  fi
+  echo $filedir
+   
+  # if folder files have not been changed.
+
+  cd $fdir
+  echo ${fdir} 
+  #rename the dat file to rawdata_suid_$suid_seuid_$seuid.dat
+  find ./ -type f -iname '*.dat' -print0 | while read -d $'\0' datfile
+  do
+     echo $datfile
+      
+     ## for now the filenumber is in the 10:3 meas_MID00259_FID00294_T2w_SPACE.dat
+     #filenumer=(${datfile:10:3})
+     #echo "filenumber : ${filenumber}"
+
+     if [ "$(( $(date +"%s") - $(stat -c "%Y" "$datfile") ))" -lt "$oldtime" ]; then
+        echo "`date`: too young $datfile"
+        continue
+     fi
+
+     suid=$(grep -a -m 1 StudyInstanceUID ${datfile} | cut -d"\"" -f4)
+     echo $suid
+     
+
+     fname=${suid}*.json
+     echo $fname
+
+     found=0
+
+     find "/data/quarantine/" -type f -iname $fname -print0 | while read -d $'\0' jsonfile
+     do
+         private0021_1106=`cat $jsonfile | jq -r ".Private0021_1106"`;
+         seriesDecr=`cat $jsonfile | jq -r ".SeriesDescription"`;
+	 echo $jsonfile
+	 echo "$private0021_1106 $seriesDecr "
+         seuid='undefined'
+
+	 if [[ $datfile =~ "MID00"${private0021_1106} ]]; then
+		if [[ $datfile =~ ${seriesDecr} ]]; then
+		    seuid=`cat $jsonfile | jq -r ".SeriesInstanceUID"`
+                    echo "Found SeUID : ${seuid}"
+	            found=1
+	        fi
+	 fi
+
+	 echo ${seuid}
+         # check /data/DAIC if the data is not in /data/quarantine
+         if [ ${seuid} = "undefined" ]; then
+             find "/data/DAIC/"  -type f -iname $fname -print0 | while read -d $'\0' jsonfile
+             do
+                 echo $jsonfile
+                 private0021_1106=`cat $jsonfile | jq -r ".Private0021_1106"`;
+                 seriesDecr=`cat $jsonfile | jq -r ".SeriesDescription"`;
+                 if [[ $datfile =~ "MID00"${private0021_1106} ]]; then
+		    if [[ $datfile =~ ${seriesDecr} ]]; then
+                        seuid=`cat $jsonfile | jq -r ".SeriesInstanceUID"`;
+                        echo "Found SeUID : ${seuid}"
+		        found=1
+                    fi
+		 fi
+             done
+         fi
+         echo ${seuid}
+         
+	 ## if both suid and seuid existed
+         if [ ${seuid} = "undefined" ]; then
+	    echo "Could not find the Seuid"
+         else
+             #seuid=$(grep -a -m 1  ${datfile} | cut -d"\"" -f4)
+             newfile="rawdata_suid_${suid}_seuid_${seuid}.dat"
+             echo $newfile
+             #zip[[ $datfile =~ ${seriesDecr} ]] the file
+             /bin/mv  $datfile $newfile || error_exit
+         fi
+         if [[ ${found} = 1 ]]; then
+             break;
+         fi
+     done
+
+     #if [[ ${found} = 1 ]]; then
+     #    break;
+     #fi
+  done
+
+  #clean up the folder
+  #rm -rf  ${kspaceDatLocations}/${filedir}
+
+  echo "Before rsyc: rawdata_suid_${suid}*.zip"
+  mv /data/site/kspace/${filedir} /data/site/kspace/processed/
+
+  #rsync this files
+  #/usr/bin/rsync -LptgoDv0 --no-R /data/site/kspace/outbox/rawdata_suid_${suid}*.zip hbcd_${user}_fiona@${endpoint}:/home/hbcd_${user}_fiona 
+  #register the files to UMN
+  #/usr/bin/python /var/www/html/server/bin/registerRawFileUpload.py --filename=rawdata_suid_${suid} --token=$token >> $log 2>&1
+done
